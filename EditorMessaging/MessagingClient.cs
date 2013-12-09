92e50a12f752463bdc349dfd2b7f3c7d10e54651
= <jaakko@hypermurea.com>
2013-12-09 22:24:29 +0200

    initial commit

---
+++ EditorMessaging/MessagingClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnityMessaging
+{
+    public class MessagingClient
+    {
+        private static MessagingClient clientInstance = new MessagingClient();
+
+        public static MessagingClient Client {
+            get {
+                return clientInstance;
+            }
+        }
+
+        public void SendMessage(string key, string value) {
+            // do nothing
+        }
+
+    }
+}
---
+++ WP8Messaging/MessagingClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace UnityMessaging {
+    public class MessagingClient {
+        private static MessagingClient clientInstance = new MessagingClient();
+
+        public static MessagingClient Client {
+            get {
+                return clientInstance;
+            }
+        }
+
+        public delegate void MessageReceived(string key, string value);
+        public event MessageReceived NewMessageReceived;
+
+
+        public void SendMessage(string key, string value) {
+            if(NewMessageReceived != null) {
+                NewMessageReceived(key, value);
+            }
+        }
+
+    }
+}
